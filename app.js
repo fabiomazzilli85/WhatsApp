eeaf6b6cbd9ddb18aa3b7e0b82c25206fa25a7eb
fabiomazzilli85 <fabio_mazzili@outlook.it>
2024-04-03 12:37:16 +0200

    Ho creato un array di oggetti conententi due proprietà: name e percorso immagine.

---
+++ app.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      contacts: [
+        { name: 'Michele', image: 'img/avatar_1.jpg' },
+        { name: 'Pippo', image: 'img/avatar_2.jpg' },
+        { name: 'Gianni', image: 'img/avatar_3.jpg' },
+        { name: 'Sara', image: 'img/avatar_4.jpg' },
+        { name: 'Fabio', image: 'img/avatar_5.jpg' },
+        { name: 'Gianluca', image: 'img/avatar_6.jpg' },
+        { name: 'Ettore', image: 'img/avatar_7.jpg' }
+      ]
+    };
+  }
+}).mount('#app');
