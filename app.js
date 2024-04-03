3193589d7cb8037fa6ed1b25ab3f2e1f2ef732df
fabiomazzilli85 <fabio_mazzili@outlook.it>
2024-04-03 17:14:15 +0200

    Creato un array contenente due oggetti (name e image) e un altro array al suo interno (messaggi).

--- app.js
+++ app.js
@@ -4,28 +4,52 @@ createApp({
   data() {
     return {
       contacts: [
-        { name: 'Michele', image: 'img/avatar_1.jpg' },
-        { name: 'Pippo', image: 'img/avatar_2.jpg' },
-        { name: 'Gianni', image: 'img/avatar_3.jpg' },
-        { name: 'Sara', image: 'img/avatar_4.jpg' },
-        { name: 'Fabio', image: 'img/avatar_5.jpg' },
-        { name: 'Gianluca', image: 'img/avatar_6.jpg' },
-        { name: 'Ettore', image: 'img/avatar_7.jpg' }
-      ],
-      activeName: null,
-      sentMessages: [
-        'Ciao! Come va?',
-        'Sto preparando il pranzo.'
-      ],
-      receivedMessages: [
-        'Domani ci vediamo al parco?',
-        'Buonanotte!'
+        {
+          name: 'Alice',
+          image: 'img/avatar_1.jpg',
+          messaggi: [
+            "Ciao! Come stai?",
+            "Hai sentito la notizia di oggi?",
+            "Ci vediamo domani?"
+          ]
+        },
+        {
+          name: 'Bob',
+          image: 'img/avata_2.jpg',
+          messaggi: [
+            "Hey, tutto bene?",
+            "Che ne dici di fare una passeggiata questo fine settimana?",
+            "Hai visto il nuovo film?"
+          ]
+        },
+        {
+          name: 'Diego',
+          image: 'img/avatar_3.jpg',
+          messaggi: [
+            "Che stai facendo?",
+            "Sei andato al cinema ieri sera?",
+            "Mi sono trovato molto bene con te"
+          ]
+        },
+        {
+          name: 'Stefania',
+          image: 'img/avatar_4.jpg',
+          messaggi: [
+            "Davvero bella Genova come città",
+            "Ho mangiato il pesto genovese. Meraviglioso",
+            "La focaccia che preparano a Genova è immangiabile."
+          ]
+        },
+        {
+          name: 'Charlie',
+          image: 'img/avatar_5.jpg',
+          messaggi: [
+            "Ciao a tutti!",
+            "Qualcuno ha piani per il prossimo weekend?",
+            "Ho appena finito un buon libro, consiglio a tutti!"
+          ]
+        }
       ]
-    };
-  },
-  methods: {
-    setActiveName(name) {
-      this.activeName = name;
     }
   }
 }).mount('#app');
