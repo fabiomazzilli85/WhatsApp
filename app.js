b0cad942af782277e47de6f139dbfe877a8bfde5
fabiomazzilli85 <fabio_mazzili@outlook.it>
2024-04-04 10:53:45 +0200

    Ho riscritto il file JS attenendomi alle indicazioni fornite.

--- app.js
+++ app.js
@@ -5,57 +5,167 @@ createApp({
     return {
       contacts: [
         {
-          name: 'Martin',
-          image: 'img/avatar_1.jpg',
-          messaggi: [
-            "Ciao! Come stai?",
-            "Hai sentito la notizia di oggi?",
-            "Ci vediamo domani?"
+          name: 'Michele',
+          avatar: './img/avatar_1.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Hai portato a spasso il cane?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Ricordati di stendere i panni',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 16:15:22',
+              message: 'Tutto fatto!',
+              status: 'received'
+            }
           ]
         },
         {
-          name: 'Bob',
-          image: 'img/avatar_2.jpg',
-          messaggi: [
-            "Hey, tutto bene?",
-            "Che ne dici di fare una passeggiata questo fine settimana?",
-            "Hai visto il nuovo film?"
+          name: 'Fabio',
+          avatar: './img/avatar_2.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '20/03/2020 16:30:00',
+              message: 'Ciao come stai?',
+              status: 'sent'
+            },
+            {
+              date: '20/03/2020 16:30:55',
+              message: 'Bene grazie! Stasera ci vediamo?',
+              status: 'received'
+            },
+            {
+              date: '20/03/2020 16:35:00',
+              message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+              status: 'sent'
+            }
           ]
         },
         {
-          name: 'Diego',
-          image: 'img/avatar_3.jpg',
-          messaggi: [
-            "Che stai facendo?",
-            "Sei andato al cinema ieri sera?",
-            "Mi sono trovato molto bene con te"
+          name: 'Samuele',
+          avatar: './img/avatar_3.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '28/03/2020 10:10:40',
+              message: 'La Marianna va in campagna',
+              status: 'received'
+            },
+            {
+              date: '28/03/2020 10:20:10',
+              message: 'Sicuro di non aver sbagliato chat?',
+              status: 'sent'
+            },
+            {
+              date: '28/03/2020 16:15:22',
+              message: 'Ah scusa!',
+              status: 'received'
+            }
           ]
         },
         {
-          name: 'Woody',
-          image: 'img/avatar_4.jpg',
-          messaggi: [
-            "Davvero bella Genova come città",
-            "Ho mangiato il pesto genovese. Meraviglioso",
-            "La focaccia che preparano a Genova è immangiabile."
+          name: 'Alessandro B.',
+          avatar: './img/avatar_4.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Lo sai che ha aperto una nuova pizzeria?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Si, ma preferirei andare al cinema',
+              status: 'received'
+            }
           ]
         },
         {
-          name: 'Charlie',
-          image: 'img/avatar_5.jpg',
-          messaggi: [
-            "Ciao a tutti!",
-            "Qualcuno ha piani per il prossimo weekend?",
-            "Ho appena finito un buon libro, consiglio a tutti!"
+          name: 'Alessandro L.',
+          avatar: './img/avatar_5.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Ricordati di chiamare la nonna',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Va bene, stasera la sento',
+              status: 'received'
+            }
+          ]
+        },
+        {
+          name: 'Claudia',
+          avatar: './img/avatar_6.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Ciao Claudia, hai novità?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Non ancora',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020 15:51:00',
+              message: 'Nessuna nuova, buona nuova',
+              status: 'sent'
+            }
+          ]
+        },
+        {
+          name: 'Federico',
+          avatar: './img/avatar_7.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Fai gli auguri a Martina che è il suo compleanno!',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Grazie per avermelo ricordato, le scrivo subito!',
+              status: 'received'
+            }
+          ]
+        },
+        {
+          name: 'Davide',
+          avatar: './img/avatar_8.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Ciao, andiamo a mangiare la pizza stasera?',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:51:00',
+              message: 'OK!!',
+              status: 'received'
+            }
           ]
         }
-      ],
-      activeContact: null
-    }
-  },
-  methods: {
-    toggleMessages(index) {
-      this.activeContact = this.activeContact === index ? null : index;
+      ]
     }
   }
 }).mount('#app');
